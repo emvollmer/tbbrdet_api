b9f9f1f40dc5b671656079ce71c009e893af9a5a
emvollmer <elena.vollmer@kit.edu>
2023-07-19 17:54:37 +0200

    Ensure venv is used in TBBRDet installation procedure, corrected yaml installation

--- install_TBBRDet.sh
+++ install_TBBRDet.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# Check for activated venv %%%%%%%%%%%%%%%%%%%% activated venv not necessary for function in image
-#if [[ "$VIRTUAL_ENV" != "" ]]; then
-#   echo "Currently in an activated virtual environment. Setup may proceed."
-#else
-#   echo "Currently not in an activated virtual environment. Must set one up to proceed with package installation..."
-#   read -p "Please provide the directory of a venv you want to activate and use: " venv_dir
-#   venv_pth="$venv_dir"/bin/activate
-#   if test -f "$venv_pth"; then
-#      echo "'$venv_pth' exists and is a venv. Activating..."
-#      source "$venv_pth"
-#   else
-#      echo "'$venv_pth' does not exist. Please create a venv to use and activate it or do so by rerunning this script."; sleep 5
-#      exit
-#   fi
-#fi
-
 
 check_correct_version () {
   local what=$1
@@ -42,6 +26,17 @@ current_cuda=$(nvcc --version | grep 'release' | awk '{print $5}' | cut -d ',' -
 required_cuda="11.6"
 check_correct_version "CUDA" $required_cuda "$current_cuda"
 
+# Check for activated venv
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+   echo "Currently in an activated virtual environment. Setup may proceed."
+else
+   echo "Currently not in an activated virtual environment. Must set one up to proceed with package installation..."
+   venv_name="venv"
+   python3.6 -m venv "$venv_name"
+   echo "Virtual environment successfully created. Activating..."
+   source "$venv_name"/bin/activate
+fi
+
 # Ensure packages are installed in the correct order
 # Make sure pip is up to date
 pip3 install --upgrade pip setuptools wheel
@@ -56,7 +51,7 @@ mim install mmcv-full==1.5
 pip3 install mmdet==2.21.0
 
 # Install everything else
-pip3 install future tensorboard mlflow joblib tqdm zstandard aiohttp yaml
+pip3 install future tensorboard mlflow joblib tqdm zstandard aiohttp pyyaml
 
 echo "Packages installed."
 
