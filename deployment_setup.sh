856964acd33c37009e6d5f6ad9880b3f2f7cf82a
emvollmer <elena.vollmer@kit.edu>
2023-07-27 14:54:13 +0200

    Updated bash script for setup according to working installation in ai4eosc deployment

--- deployment_setup.sh
+++ deployment_setup.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+# ########## Connect remotely to NextCloud (rshare)
+if rclone listremotes | grep -q "rshare:" ; then
+    echo "Rshare identified as remote. Obscuring password..."
+
+    if rclone about rshare: | grep -q "Used:" ; then
+        echo "Successful connection to remote rshare."
+    else
+        echo export RCLONE_CONFIG_RSHARE_PASS=$(rclone obscure $RCLONE_CONFIG_RSHARE_PASS) >> /root/.bashrc
+        source /root/.bashrc
+        echo "Error in connecting to remote rshare."; sleep 10
+        exit 1
+    fi
+else
+    echo "Rshare not identified as (only) remote. Try to solve manually with AI4EOSC documentation."
+    sleep 10
+    exit 1
+fi
+
 # Function to install CUDA and CUDNN
 install_cuda_cudnn() {
     # install cuda: https://gist.github.com/ksopyla/bf74e8ce2683460d8de6e0dc389fc7f5
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
     mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+    apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub   # new key, added 2022-04-25 22:52
     add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 
     # install cuda toolkit
     apt update
-    apt install cuda-toolkit-11-6
+    yes | apt install cuda-toolkit-11-6
 
     # install cudnn
     apt-get install libcudnn8=8.4.0.27-1+cuda11.6
@@ -19,28 +37,40 @@ install_cuda_cudnn() {
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
     export CUDA_HOME=/usr/local/cuda
     export PATH="/usr/local/cuda/bin:$PATH"
-    # export CUDA_HOME=/usr/local/cuda-11.6
-    # export PATH=/usr/local/cuda-11.6/bin${PATH:+:${PATH}}
+
+    # check correct cuda location
+    if which nvcc | grep -q "/usr/local/cuda/" ; then
+        echo "CUDA and CUDNN installation successful."
+    else
+        echo "CUDA and CUDNN installation failed. Please check manually."; sleep 10
+        exit 1
+    fi
 }
 
 
 # ########### Install required CUDA and CUDNN versions
 # check if cuda is installed
+required_cuda="11.6"
 if ! command -v "nvcc --version" &> /dev/null ; then
-    echo "NVCC not found. Installing CUDA 11.6..."
+    echo "NVCC not found. Installing CUDA $required_cuda..."
     install_cuda_cudnn
 else
-    echo "CUDA exists"
-    # check version, delete if not 11.6
-    # then: install_cuda_cudnn
+    current_cuda=$(nvcc --version | grep 'release' | awk '{print $5}' | cut -d ',' -f 1)
+    if [[ "$required_cuda" == "$current_cuda" ]]; then
+        echo "CUDA exists, with CUDA version requirement $required_cuda satisfied."
+    else
+        echo "CUDA exists, but CUDA version $current_cuda is not the required $required_cuda.
+        Installing $required_cuda..."
+        install_cuda_cudnn
+    fi
 fi
 
 # ########### Install python3.6
-if ! which python3.6 &>/dev/null; then
+if ! which python3.6 &> /dev/null; then
     echo "Python 3.6 is not installed."
-    add-apt-repository ppa:deadsnakes/ppa
+    yes | add-apt-repository ppa:deadsnakes/ppa
     apt-get update
-    apt-get install python3.6
+    yes | apt-get install python3.6
 fi
 
 # in ubuntu 20.04 base image, python is undefined, python3 is defined as 3.8
