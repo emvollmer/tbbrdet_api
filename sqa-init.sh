26b96b704ecff47aeb41828ea7e9efaa23674e60
valentin.kozlov <valentin.kozlov@kit.edu>
2023-12-17 01:41:54 +0100

    [cicd]: Update sqa-init.sh to avoid dubios ownership

--- sqa-init.sh
+++ sqa-init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# git config --global --add safe.directory /srv/tbbrdet_api/* && \
-git config --global --add safe.directory '/srv/*' && \
+git config --global --add safe.directory /srv/tbbrdet_api && \
+git config --global --add safe.directory /srv/tbbrder_api/TBBRDet && \
 git submodule init && \
 git submodule update --remote --merge && \
 pip3 install -e ./TBBRDet && \
- pip3 install -e . && \
+pip3 install -e . && \
 pip3 install -r requirements-test.txt
